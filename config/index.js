3003c7b87abb3f60633c9b849d16e6f1d6ed5b35
Alex Hernandez <alexandre_2420@outlook.com>
2021-04-05 17:05:58 -0500

    Conexión a MongoAtlas una instancia de MongoDB

--- config/index.js
+++ config/index.js
@@ -2,7 +2,15 @@ require('dotenv').config()
 
 const config = {
     dev: process.env.NODE_ENV !== 'production',
-    port: process.env.NODE_PORT || 3006
+    port: process.env.NODE_PORT || 3006,
+
+    //CONFIG
+    cors: process.env.CORS,
+
+    //MONGO
+    dbUser: process.env.BD_USER,
+    dbPassword: process.env.BD_PASSWORD,
+    dbName: process.env.BD_NAME
 }
 
 module.exports = { config }
