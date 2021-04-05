4c438750b0f8afcf3705b5737725ac59f6a6b928
Alex Hernandez <alexandre_2420@outlook.com>
2021-04-05 14:50:30 -0500

    Métodos idempotentes del CRUD

--- routes/movies.js
+++ routes/movies.js
@@ -18,6 +18,58 @@ function moviesApi(app) {
             console.error(chalk.red("[routes/movies] ", error.message))
         }
     })
+
+    router.get("/:moviedId", async (req, res, next) => {
+        try {
+            const movies = await Promise.resolve(moviesMock[0])
+
+            res.status(200).json({
+                data: movies,
+                message: 'movies retrieve'
+            })
+        } catch (error) {
+            console.error(chalk.red("[routes/movies] ", error.message))
+        }
+    })
+
+    router.post("/", async (req, res, next) => {
+        try {
+            const idMovie = await Promise.resolve(moviesMock[0].id)
+
+            res.status(201).json({
+                data: idMovie,
+                message: 'movies created'
+            })
+        } catch (error) {
+            console.error(chalk.red("[routes/movies] ", error.message))
+        }
+    })
+
+    router.put("/:movieId", async (req, res, next) => {
+        try {
+            const idMovie = await Promise.resolve(moviesMock[0].id)
+
+            res.status(200).json({
+                data: idMovie,
+                message: 'movie update'
+            })
+        } catch (error) {
+            console.error(chalk.red("[routes/movies] ", error.message))
+        }
+    })
+
+    router.delete("/:movieId", async (req, res, next) => {
+        try {
+            const idMovie = await Promise.resolve(moviesMock[0].id)
+
+            res.status(200).json({
+                data: idMovie,
+                message: 'movies deleted'
+            })
+        } catch (error) {
+            console.error(chalk.red("[routes/movies] ", error.message))
+        }
+    })
 }
 
 module.exports = moviesApi
