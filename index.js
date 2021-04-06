9713b74ab5c794dae5621aae7817dc2d38931be9
Alex Hernandez <alexandre_2420@outlook.com>
2021-04-06 11:17:53 -0500

    Pimplementando helemt

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const chalk = require('chalk')
 const cors = require('cors')
+const helmet = require('helmet')
 
 //CONFIG
 const { config } = require('./config')
@@ -20,8 +21,9 @@ const app = express()
 //MIDDLEWARE PARA RECIBIR JSON
 app.use(express.json())
 
-//CORS
+//MIDDLEARES POUPULARES
 app.use(cors())
+app.use(helmet())
 
 //ROUTES
 moviesApi(app)
