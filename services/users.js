92a62d48951a6a7f6d4186727363347fd09b7642
Alex Hernandez <alexandre_2420@outlook.com>
2021-04-08 15:35:47 -0500

    Agregando la colección de usuarios

---
+++ services/users.js
@@ -0,0 +1,45 @@
+const chalk = require('chalk')
+const MongoLib = require('../lib/mongo')
+
+const bcrypt = require('bcrypt')
+
+class UserService {
+    constructor() {
+        this.collection = 'users'
+        this.MongoDB = new MongoLib()
+    }
+
+    async getUser({ email } ) {
+        try {
+            const [ user ] = await this.MongoDB.get(this.collection, { email })
+            return user
+        } catch (error) {
+            console.error(chalk.red("[GETUSERS]: ", error.message))
+        }
+    }
+
+    async createUser( { user } ) {
+        try {
+            const { name, email, password } = user
+            //PARA VALIDAR SI EXISTE ESE CORREO EN LA DB Y NO CRERLO DE NUEVO
+            const queriedUser = await this.getUser({ email: user.email })
+
+            if(!queriedUser) {
+                const hashedPassword = await bcrypt.hash(password, 6)
+                const createUserId = await this.MongoDB.create(this.collection, { 
+                    name,
+                    email,
+                    password: hashedPassword
+                })
+
+                return createUserId
+            } else {
+                return 'user already  exist'
+            }
+        } catch (error) {
+            console.error(chalk.red("[CREATEUSERS]: ", error.message))
+        }
+    }
+}
+
+module.exports = UserService
